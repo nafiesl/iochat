ee35decfa89cf647f93d577fedc8b6795071cd81
Nafies Luthfi <nafiesl@gmail.com>
2018-01-24 09:40:47 +0800

    Add disconnect socket detection

--- socket.js
+++ socket.js
@@ -12,7 +12,12 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.sockets.on('connection', function (soccet) {
-    connections.push(soccet);
-    console.log('Connected: %s soccets connected.', connections.length);
+io.sockets.on('connection', function (socket) {
+    connections.push(socket);
+    console.log('Connected: %s sockets connected.', connections.length);
+
+    socket.on('disconnect', function (data) {
+        connections.splice(connections.indexOf(socket), 1);
+        console.log('Disconnected: %s sockets connected.', connections.length);
+    });
 });
