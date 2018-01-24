b5dcd251f1a635f3974ac6b3cac61279528ba4ab
Nafies Luthfi <nafiesl@gmail.com>
2018-01-24 10:30:06 +0800

    Add user login form and update username to user online list
    Add user login form
    Add username to user online list after logged in
    Add usermame list update (removal) on page closed
    Prevent user from send message if they not logged in

--- socket.js
+++ socket.js
@@ -18,6 +18,8 @@ io.sockets.on('connection', function (socket) {
 
     // Disconnect
     socket.on('disconnect', function (data) {
+        users.splice(users.indexOf(socket.username), 1);
+        updateUsernames();
         connections.splice(connections.indexOf(socket), 1);
         console.log('Disconnected: %s sockets connected.', connections.length);
     });
@@ -26,4 +28,16 @@ io.sockets.on('connection', function (socket) {
     socket.on('send message', function (data) {
         io.sockets.emit('new message', {msg: data});
     });
+
+    // User login
+    socket.on('login', function (data, callback) {
+        callback(true);
+        socket.username = data;
+        users.push(socket.username);
+        updateUsernames();
+    });
+
+    function updateUsernames() {
+        io.sockets.emit('get users', users);
+    }
 });
