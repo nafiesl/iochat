08f75f3176d7536e4ce2f6c36453e8d757fb2543
Nafies Luthfi <nafiesl@gmail.com>
2018-01-24 09:54:22 +0800

    Send and receive message
    index.hmtl send message to server (socket.js) by "send message" emit
    socket.js receives "send message" and emit "new message" with data
    index.html receives "new message" and append chat box with data.msg

--- socket.js
+++ socket.js
@@ -16,8 +16,14 @@ io.sockets.on('connection', function (socket) {
     connections.push(socket);
     console.log('Connected: %s sockets connected.', connections.length);
 
+    // Disconnect
     socket.on('disconnect', function (data) {
         connections.splice(connections.indexOf(socket), 1);
         console.log('Disconnected: %s sockets connected.', connections.length);
+    });
+
+    // Send Message
+    socket.on('send message', function (data) {
+        io.sockets.emit('new message', {msg: data});
     });
 });
